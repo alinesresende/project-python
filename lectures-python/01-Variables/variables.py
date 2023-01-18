1968769c510a38914a2489862b320b6d597e5065
Aline <alinesresende14@gmail.com>
2023-01-18 15:10:38 -0300

    Study of variables in the Python language.

---
+++ lectures-python/01-Variables/variables.py
@@ -0,0 +1,11 @@
+# Python language
+print('Hello world!')
+
+# Declaration of variables
+name = 'Humberto Delgado'
+company = 'FIAP'
+numberOfEmployees = 500
+averageTuition = 856.50
+print(name + " works at the company " + company)
+print("It has: ", numberOfEmployees, " employees.")
+print("The average monthly fee is: " + str(averageTuition))
