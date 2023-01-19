3c7da6a638c9b99e53dd8d1baf6b05fe2a2b21d6
Aline <alinesresende14@gmail.com>
2023-01-19 10:50:36 -0300

    Decision content with if.

--- lectures-python/01-Variables/variables.py
+++ lectures-python/01-Variables/variables.py
@@ -9,3 +9,7 @@ averageTuition = 856.50
 print(name + " works at the company " + company)
 print("It has: ", numberOfEmployees, " employees.")
 print("The average monthly fee is: " + str(averageTuition))
+
+# Input
+
+# The input only works with string, to work with numbers it is necessary to use "int" or "float" before the "input"
---
+++ lectures-python/02-Decisions-with-if/decisions.py
@@ -0,0 +1,9 @@
+# Decisions with if
+print('Hello world!')
+
+name = 'Aline'
+age = 26
+if age <= 65:
+  print("The patient " + name + " no priority service!")
+else:
+   print("The patient" + name + "priority service!")
